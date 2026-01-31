6b6231e17fa1c0945a1c524d772eb9990df958b3
Michael Goodnow <mmgoodnow@gmail.com>
2026-01-30 18:02:49 -0800

    Embed YouTube trailers with HTML wrapper

--- Badminton/Shared/TrailerPlayerView.swift
+++ Badminton/Shared/TrailerPlayerView.swift
@@ -52,9 +52,7 @@ private struct WebView: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        if uiView.url != url {
-            uiView.load(URLRequest(url: url))
-        }
+        loadContent(into: uiView, url: url)
     }
 }
 #else
@@ -69,9 +67,58 @@ private struct WebView: NSViewRepresentable {
     }
 
     func updateNSView(_ nsView: WKWebView, context: Context) {
-        if nsView.url != url {
-            nsView.load(URLRequest(url: url))
-        }
+        loadContent(into: nsView, url: url)
     }
 }
 #endif
+
+private func loadContent(into webView: WKWebView, url: URL) {
+    if let embedID = youtubeEmbedID(from: url) {
+        let html = """
+        <!doctype html>
+        <html>
+          <head>
+            <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" />
+            <style>
+              html, body { margin: 0; padding: 0; background: #000; height: 100%; }
+              iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0; }
+            </style>
+          </head>
+          <body>
+            <iframe
+              src=\"https://www.youtube.com/embed/\(embedID)?playsinline=1&autoplay=1&rel=0&modestbranding=1&origin=https://www.youtube.com\"
+              allow=\"autoplay; encrypted-media; picture-in-picture\"
+              allowfullscreen
+              referrerpolicy=\"origin\">
+            </iframe>
+          </body>
+        </html>
+        """
+        webView.loadHTMLString(html, baseURL: URL(string: "https://www.youtube.com"))
+        return
+    }
+
+    if webView.url != url {
+        webView.load(URLRequest(url: url))
+    }
+}
+
+private func youtubeEmbedID(from url: URL) -> String? {
+    guard let host = url.host?.lowercased() else { return nil }
+
+    if host.contains("youtu.be") {
+        return url.pathComponents.last
+    }
+
+    if host.contains("youtube.com") {
+        if url.path.contains("/embed/") {
+            return url.pathComponents.last
+        }
+        if let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+           let id = components.queryItems?.first(where: { $0.name == "v" })?.value {
+            return id
+        }
+    }
+
+    return nil
+}
