3e25455e079ce8bc51484c29e20d2a3a52ce7074
Michael Goodnow <mmgoodnow@gmail.com>
2026-02-03 22:29:19 -0800

    Add editable Overseerr site name

--- Badminton/Overseerr/OverseerrAuthManager.swift
+++ Badminton/Overseerr/OverseerrAuthManager.swift
@@ -3,6 +3,12 @@ import Foundation
 
 @MainActor
 final class OverseerrAuthManager: ObservableObject {
+    @Published var siteName: String {
+        didSet {
+            let trimmed = siteName.trimmingCharacters(in: .whitespacesAndNewlines)
+            storage.save(trimmed, for: .siteName)
+        }
+    }
     @Published var baseURLString: String {
         didSet {
             let trimmed = baseURLString.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -27,6 +33,7 @@ final class OverseerrAuthManager: ObservableObject {
         self.client = client
         let storedBaseURL = storage.read(.baseURL) ?? ""
         self.baseURLString = storedBaseURL
+        self.siteName = storage.read(.siteName) ?? ""
         self.sessionCookie = storage.read(.sessionCookie)
         self.userDisplayName = storage.read(.userDisplayName)
         self.isAuthenticated = sessionCookie != nil
@@ -81,6 +88,7 @@ final class OverseerrAuthManager: ObservableObject {
 private struct OverseerrTokenStore {
     enum Key: String {
         case baseURL = "overseerr.base.url"
+        case siteName = "overseerr.site.name"
         case sessionCookie = "overseerr.session.cookie"
         case userDisplayName = "overseerr.user.name"
     }
